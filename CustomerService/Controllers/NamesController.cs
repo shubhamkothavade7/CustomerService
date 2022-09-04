22c774600edd1e06d15106f96f75ca0b557d693a
shubhamkothavade7 <shubhamkothavade7@gmail.com>
2022-09-04 16:22:58 +0530

    NamesController added by shubham

---
+++ CustomerService/Controllers/NamesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NamesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> get()
+        {
+            return new string[] {"ram","shyam","anil"};
+        }
+        /*[HttpGet]
+        public string get(int id)
+        {
+            return "value";
+        }*/
+    }
+}
